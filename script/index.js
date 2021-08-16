1f9e6990c80b1cc433f19e86b2cdc9bb06e9bd60
wowankz <wowikwow@mail.ru>
2021-08-17 02:21:24 +0600

    update header

--- script/index.js
+++ script/index.js
@@ -1,8 +1,13 @@
-const modal_cookie = document.querySelector('.modal-cookie');
-modal_cookie.onload =
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const modal_cookie = document.querySelector('.modal-cookie');
+    modal_cookie.style.transform = 'scale(1)'
     modal_cookie.addEventListener('click', (event) => {
         if (event.target.id === 'close-cookie') {
             console.log(event.target);
             modal_cookie.style.transform = 'scale(0)'
         }
-    })
+    });
+    console.log('load');
+})
