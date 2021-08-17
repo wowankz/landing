75b7c28aee7978135ffcd3ed1b0f88558e4d0dc1
wowankz <wowikwow@mail.ru>
2021-08-17 18:46:27 +0600

    сделал появление модального окна cookie xthtp ctreyle

--- script/index.js
+++ script/index.js
@@ -2,7 +2,8 @@
 
 window.addEventListener('load', function () {
     const modal_cookie = document.querySelector('.modal-cookie');
-    modal_cookie.style.transform = 'scale(1)'
+    this.setTimeout(() => { modal_cookie.style.transform = 'scale(1)' }, 1000)
+
     modal_cookie.addEventListener('click', (event) => {
         if (event.target.id === 'close-cookie') {
             console.log(event.target);
