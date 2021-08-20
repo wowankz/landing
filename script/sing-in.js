4a289e755733096345e44a45c00e59eb995fb9c2
wowankz <wowikwow@mail.ru>
2021-08-21 02:27:54 +0600

    add modal policy

---
+++ script/sing-in.js
@@ -0,0 +1,15 @@
+const modal_policy = document.querySelector('.modal-policy');
+const btn_policy = document.querySelector('.accept__link');
+const btn_close = document.querySelector('.modal-policy__close')
+
+btn_policy.addEventListener('click', (event) => {
+    modal_policy.style.display = 'block';
+    setTimeout(() => { modal_policy.style.transform = 'scale(1)'; }, 300)
+
+})
+
+btn_close.addEventListener('click', (event) => {
+    modal_policy.style.transform = 'scale(0)';
+    setTimeout(() => { modal_policy.style.display = 'none'; }, 300)
+
+})
