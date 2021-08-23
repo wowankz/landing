8411dd7fc2bf32bf3cff7c06956b79f1cc410070
wowankz <wowikwow@mail.ru>
2021-08-23 06:11:58 +0600

    add modal manage

--- script/index.js
+++ script/index.js
@@ -1,16 +1,80 @@
+let modal_cookie = null;
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function closeModalCookie() {
+    modal_cookie.style.transform = 'scale(0)';
+    setTimeout(() => { modal_cookie.remove(); }, 500)
 
 
-window.addEventListener('load', function () {
-    const modal_cookie = document.querySelector('.modal-cookie');
-    this.setTimeout(() => { modal_cookie.style.transform = 'scale(1)' }, 1000)
+}
+
+function getHtmlModalCookie() {
+    return `
+    <div class="modal-cookie">
+        <img id="close-cookie" src="./img/modal-close-icon.svg" alt="Close icon" class="modal-cookie__close">
+        <p class="modal-cookie__text">
+            We use cookies to improve your experience and for marketing. Learn more in our cookie policy.
+        </p>
+        <div class="modal-cookie__buttons">
+            <button id="manage-cookie" class="modal-cookie__button-manage">Manage cookies</button>
+            <button id="accept-all" class="modal-cookie__button-accept">Accept All</button>
+        </div>
+    </div>
+  `
+}
+
+function showModalCookie() {
+    document.querySelector('body').insertAdjacentHTML('beforeend', getHtmlModalCookie());
+    modal_cookie = document.querySelector('.modal-cookie');
+    setTimeout(() => {
+        if (!getCookie('visit'))
+            modal_cookie.style.transform = 'scale(1)'
+    }, 1000)
 
     modal_cookie.addEventListener('click', (event) => {
         if (event.target.id === 'close-cookie') {
             console.log(event.target);
-            modal_cookie.style.transform = 'scale(0)'
+            closeModalCookie()
+        }
+
+        if (event.target.id == 'accept-all') {
+            console.log('Accept all cookie');
+            document.cookie = 'visit=true; path=/;max-age=60';
+            closeModalCookie();
+        }
+
+        if (event.target.id === 'manage-cookie') {
+            console.log('Manage cookie');
+            const modal_manage = document.querySelector('.modal-manage');
+            closeModalCookie();
+            modal_manage.classList.add('modal_show');
+            modal_manage.addEventListener('click', (event) => {
+                if (event.target.classList.contains('modal-manage__info-btn')) {
+                    event.target.parentElement.classList.toggle('modal-manage__info_open');
+                    event.target.innerHTML = event.target.parentElement.classList.contains('modal-manage__info_open')
+                        ? 'Nasconi'
+                        : 'Scopri di piu';
+
+                }
+
+                if (event.target.classList.contains('modal-btn__close')) {
+                    console.log('close');
+                    modal_manage.classList.remove('modal_show');
+                    setTimeout(() => { showModalCookie(); }, 300)
+                }
+            })
         }
     });
+}
 
+function init() {
+    showModalCookie();
     const howWork = document.querySelector('.how-work__cards');
     const howWorkItems = howWork.querySelectorAll('.how-work__cards-item');
     const howWorkTabs = document.querySelectorAll('.how-work__tab');
@@ -46,4 +110,6 @@ window.addEventListener('load', function () {
             howWorkItems[6].classList.add('active')
         };
     })
-})
+}
+
+document.addEventListener('DOMContentLoaded', init)
--- script/sing-in.js
+++ script/sing-in.js
@@ -1,14 +1,14 @@
 const modal_policy = document.querySelector('.modal-policy');
 const btn_policy = document.querySelector('.accept__link');
-const btn_close = document.querySelector('.modal-policy__close')
+const btn_close = document.querySelector('.modal-btn__close')
 
-btn_policy.addEventListener('click', (event) => {
+btn_policy.addEventListener('click', () => {
     modal_policy.style.display = 'block';
     setTimeout(() => { modal_policy.style.transform = 'scale(1)'; }, 300)
 
 })
 
-btn_close.addEventListener('click', (event) => {
+btn_close.addEventListener('click', () => {
     modal_policy.style.transform = 'scale(0)';
     setTimeout(() => { modal_policy.style.display = 'none'; }, 300)
 
